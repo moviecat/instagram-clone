edf0988f1808c6e9fbc70303469b818c143e262a
Arum <gliese7777@gmail.com>
2020-04-14 10:34:27 +0900

    이메일 인증 완료 페이지 추가

--- javascript/modal_개별게시물.js
+++ javascript/modal_개별게시물.js
@@ -1,11 +1,8 @@
-// myfeed
 
 let setting = document.querySelector('.more_info');
 let modal4 = document.querySelector('.modal4');
 let overlay4 = modal4.querySelector('.md_overlay4');
 let btn = modal4.querySelector('.btn_close4');
-
-
 
 let openModal4 = () => {
     modal4.classList.remove('hidden4');
@@ -15,8 +12,6 @@ let closeModal4 = () => {
     modal4.classList.add('hidden4');
 }
 
-
-// myfeed
 setting.addEventListener("click", openModal4);
 overlay4.addEventListener("click", closeModal4);
 btn.addEventListener("click", closeModal4);
