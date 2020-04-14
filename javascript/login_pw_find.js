adbf4ec26fcde762a242c2323a657d85574c8388
Arum <gliese7777@gmail.com>
2020-04-14 16:56:43 +0900

    setting_pw 수정 (이미지 첨부 기능/소개창 수정/태그들의 id명 변경)

--- javascript/login_pw_find.js
+++ javascript/login_pw_find.js
@@ -6,3 +6,22 @@ $(function(){
             $("#pw_send").attr("disabled", false);
     });
 })
+
+// 이미지 첨부시 보이게
+$(function() {
+    $("#small_img").on('change', function(){
+        readURL(this);
+    });
+});
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+            $('#blah').attr('src', e.target.result);
+        }
+
+      reader.readAsDataURL(input.files[0]);
+    }
+}
